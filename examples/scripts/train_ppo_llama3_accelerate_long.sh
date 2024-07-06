62b4ce61b88d7a4653ba5210e59780a6d25ca151
YellowBee <yellowbee686@gmail.com>
2024-07-06 16:22:01 +0800

    add script

---
+++ examples/scripts/train_ppo_llama3_accelerate_long.sh
@@ -0,0 +1,37 @@
+set -x 
+
+export CUDA_VISIBLE_DEVICES='1,2,3,4'
+
+read -r -d '' training_commands <<EOF
+../train_ppo.py \
+    --pretrain ./ckpt/llama3_8b_bt \
+    --reward_pretrain sfairXC/FsfairX-LLaMA3-RM-v0.1 \
+    --save_path ./ckpt/llama3_8b_bt_2 \
+    --save_steps -1 \
+    --logging_steps 1 \
+    --eval_steps -1 \
+    --micro_train_batch_size 2 \
+    --micro_rollout_batch_size 4 \
+    --rollout_batch_size 1024 \
+    --max_epochs 1 \
+    --prompt_max_len 8192 \
+    --generate_max_len 2048 \
+    --zero_stage 2 \
+    --bf16 \
+    --actor_learning_rate 5e-7 \
+    --critic_learning_rate 9e-6 \
+    --init_kl_coef 0.01 \
+    --prompt_data RLHFlow/iterative-prompt-v1-iter2-20K \
+    --prompt_data_probs 1.0 \
+    --max_samples 80000 \
+    --normalize_reward \
+    --actor_init_on_gpu \
+    --adam_offload \
+    --flash_attn \
+    --gradient_checkpointing
+EOF
+     # --wandb [WANDB_TOKENS] or True (use wandb login command)
+
+
+accelerate launch --config_file ./zero2_test.yaml --main_process_port 29505 $training_commands
+
