8d01c6011a0a9ce6e9f7f3aa8607668866ebbb43
ChengXiao686 <xiao.cheng@autra.tech>
2024-07-09 12:03:43 +0800

    fix

--- examples/scripts/train_ppo_llama3_accelerate.sh
+++ examples/scripts/train_ppo_llama3_accelerate.sh
@@ -1,6 +1,6 @@
 set -x 
 
-export CUDA_VISIBLE_DEVICES='0,3'
+export CUDA_VISIBLE_DEVICES='0,1,2,3'
 
 read -r -d '' training_commands <<EOF
 ../train_ppo.py \
@@ -11,8 +11,8 @@ read -r -d '' training_commands <<EOF
     --logging_steps 1 \
     --eval_steps -1 \
     --train_batch_size 128 \
-    --micro_train_batch_size 1 \
-    --micro_rollout_batch_size 2 \
+    --micro_train_batch_size 2 \
+    --micro_rollout_batch_size 4 \
     --rollout_batch_size 1024 \
     --max_epochs 1 \
     --prompt_max_len 8192 \
@@ -22,13 +22,15 @@ read -r -d '' training_commands <<EOF
     --actor_learning_rate 5e-7 \
     --critic_learning_rate 9e-6 \
     --init_kl_coef 0.01 \
-    --prompt_data RLHFlow/test_generation_2k \
+    --prompt_data RLHFlow/iterative-prompt-v1-iter1-20K \
     --prompt_data_probs 1.0 \
     --max_samples 80000 \
     --normalize_reward \
     --actor_init_on_gpu \
     --adam_offload \
     --flash_attn \
+    --input_key context_messages \
+    --apply_chat_template \
     --save_value_network \
     --gradient_checkpointing
 EOF
