b726311b694aa72b91d3b7ebb442168167b85d6b
ChengXiao686 <xiao.cheng@autra.tech>
2024-07-09 14:35:21 +0800

    fix

--- openrlhf/utils/deepspeed.py
+++ openrlhf/utils/deepspeed.py
@@ -199,7 +199,7 @@ class DeepspeedStrategy(ABC):
             train_batch_size *= 2
         
         ds_config["train_batch_size"] = train_batch_size
-        ds_config['train_micro_batch_size_per_gpu'] = train_batch_size / self.micro_train_batch_size / self.world_size
+        ds_config['gradient_accumulation_steps'] = train_batch_size / self.micro_train_batch_size / self.world_size
 
         return ds_config
 
