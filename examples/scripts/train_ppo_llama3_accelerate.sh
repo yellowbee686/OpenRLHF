22411ca2b021f2ebc079969b957e48eb4c7d8aad
ChengXiao686 <xiao.cheng@autra.tech>
2024-07-09 17:15:28 +0800

    fix

--- examples/scripts/train_ppo_llama3_accelerate.sh
+++ examples/scripts/train_ppo_llama3_accelerate.sh
@@ -12,7 +12,7 @@ read -r -d '' training_commands <<EOF
     --eval_steps -1 \
     --train_batch_size 96 \
     --micro_train_batch_size 4 \
-    --micro_rollout_batch_size 8 \
+    --micro_rollout_batch_size 4 \
     --rollout_batch_size 768 \
     --max_epochs 1 \
     --prompt_max_len 2048 \
