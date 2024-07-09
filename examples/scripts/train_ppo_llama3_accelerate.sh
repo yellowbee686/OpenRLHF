706faff0ecfc74c1fa7e76deb35fa9a5eafbe35d
ChengXiao686 <xiao.cheng@autra.tech>
2024-07-09 21:02:29 +0800

    fix

--- examples/scripts/train_ppo_llama3_accelerate.sh
+++ examples/scripts/train_ppo_llama3_accelerate.sh
@@ -16,7 +16,7 @@ read -r -d '' training_commands <<EOF
     --rollout_batch_size 512 \
     --max_epochs 1 \
     --prompt_max_len 8192 \
-    --generate_max_len 2048 \
+    --generate_max_len 1536 \
     --zero_stage 2 \
     --bf16 \
     --actor_learning_rate 5e-7 \
