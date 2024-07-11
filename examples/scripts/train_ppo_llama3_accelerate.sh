732e44876686461ee685eab47d35e3277768b47d
ChengXiao686 <xiao.cheng@autra.tech>
2024-07-11 16:21:15 +0800

    fix

--- examples/scripts/train_ppo_llama3_accelerate.sh
+++ examples/scripts/train_ppo_llama3_accelerate.sh
@@ -1,15 +1,17 @@
 set -x 
 
-export CUDA_VISIBLE_DEVICES='1,2,3,7'
+export CUDA_VISIBLE_DEVICES='2,3,4,5'
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 read -r -d '' training_commands <<EOF
 ../train_ppo.py \
-    --pretrain RLHFlow/LLaMA3-SFT \
+    --pretrain ckpt/checkpoints_ppo/_actor \
     --reward_pretrain sfairXC/FsfairX-LLaMA3-RM-v0.1 \
+    --critic_pretrain ckpt/checkpoints_ppo/_critic \
     --save_path ./ckpt/llama3_8b_bt \
     --save_steps 2 \
     --max_ckpt_num 2 \
+    --ckpt_path ./ckpt/ppo_iter2 \
     --logging_steps 1 \
     --eval_steps -1 \
     --train_batch_size 64 \
